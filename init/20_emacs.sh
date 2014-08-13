8e024ebc801f8b169afbe68e49b20940c2cb1c9d
Ian Truslove <ian.truslove@sonian.net>
2014-08-13 09:19:30 -0600

    Emacs Live

--- init/20_emacs.sh
+++ init/20_emacs.sh
@@ -1,13 +1,2 @@
-# Clone .emacs.d
-if [[ ! -d ~/.emacs.d ]]; then
-    git clone --recursive git@github.com:iantruslove/.emacs.d.git ~/.emacs.d
-fi
-
-# Install cask
-if [[ ! -e ~/.cask/bin/cask ]]; then
-    curl -fsSkL https://raw.github.com/cask/cask/master/go | python
-fi
-
-cd ~/.emacs.d
-~/.cask/bin/cask install
-cd ~
+# Install emacs live
+bash <(curl -fksSL https://raw.github.com/overtone/emacs-live/master/installer/install-emacs-live.sh)
