cdc842e3be088d9294b01673c8759e0be421335e
Ian Truslove <ian.truslove@sonian.net>
2014-08-13 10:20:07 -0600

    Just clone it...

--- init/20_emacs.sh
+++ init/20_emacs.sh
@@ -1,2 +1,3 @@
 # Install emacs live
-bash <(curl -fksSL https://raw.github.com/overtone/emacs-live/master/installer/install-emacs-live.sh)
+[[ -d ~/.emacs.d ]] && mv ~/.emacs.d ~/.old_emacs.d
+git clone https://github.com/overtone/emacs-live.git ~/.emacs.d
