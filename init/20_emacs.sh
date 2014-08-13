2ab042a6365f9317a112b84c6fddbb0a3fddf2b9
Ian Truslove <ian.truslove@sonian.net>
2014-08-13 11:24:06 -0600

    Back to the regular emacs config
    Conflicts:
    	init/20_emacs.sh

--- init/20_emacs.sh
+++ init/20_emacs.sh
@@ -1,13 +1,3 @@
-# Clone .emacs.d
-if [[ ! -d ~/.emacs.d ]]; then
-    git clone --recursive git@github.com:iantruslove/.emacs.d.git ~/.emacs.d
-fi
+[[ -d ~/.emacs.d ]] && mv ~/.emacs.d ~/.old_emacs.d
 
-# Install cask
-if [[ ! -e ~/.cask/bin/cask ]]; then
-    curl -fsSkL https://raw.github.com/cask/cask/master/go | python
-fi
-
-cd ~/.emacs.d
-~/.cask/bin/cask install
-cd ~
+git clone https://github.com/iantruslove/.emacs.d.git ~/.emacs.d
