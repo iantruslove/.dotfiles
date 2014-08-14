35d10117eceb96b35c161c63bba3b66c2fe93f75
Ian Truslove <EMAIL_ADDRESS>
2014-08-14 05:34:31 +0000

    Immediately write bash command history

--- source/50_history.sh
+++ source/50_history.sh
@@ -4,15 +4,20 @@
 shopt -s histreedit
 # History expansions will be verified before execution.
 shopt -s histverify
+# append to history, don't overwrite it
+shopt -s histappend
 
 # Entries beginning with space aren't added into history, and duplicate
 # entries will be erased (leaving the most recent entry).
-export HISTCONTROL="ignorespace:erasedups"
+export HISTCONTROL="ignorespace:ignoredups:erasedups"
 # Give history timestamps.
 export HISTTIMEFORMAT="[%F %T] "
 # Lots o' history.
-export HISTSIZE=10000
-export HISTFILESIZE=10000
+export HISTSIZE=100000
+export HISTFILESIZE=100000
+
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 # Easily re-execute the last history command.
 alias r="fc -s"
