a6895299790af60e907e67b332e15caa60d86b2d
Ian Truslove <ian.truslove@gmail.com>
2015-04-27 00:46:53 +0000

    For real - the old emacs.d needs to go away

--- init/20_emacs.sh
+++ init/20_emacs.sh
@@ -2,4 +2,4 @@
 [[ -d ~/.emacs.d ]] && mv ~/.emacs.d ~/.old_emacs.d
 
 # Pull down emacs config
-git clone --recursive https://github.com/iantruslove/emacs.d.git ~/.emacs.d
+git clone --recursive https://github.com/iantruslove/.emacs.d.git ~/.emacs.d
