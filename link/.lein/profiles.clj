8c0d93edbd0eca1849ba996ab4134eaf6f329c22
Ian Truslove <ian.truslove@sonian.net>
2015-10-07 13:04:34 +0000

    Lein profile updates and cleanup

--- link/.lein/profiles.clj
+++ link/.lein/profiles.clj
@@ -1,9 +1,8 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :plugins [[cider/cider-nrepl "0.8.2"]
-                  [refactor-nrepl "1.0.5"]
+        :plugins [[cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "1.1.0"]
                   [lein-pprint "1.1.1"]
-                  [lein-bikeshed "0.1.7"]
-                  [lein-kibit "0.1.2"]]
+                  [lein-bikeshed "0.2.0"]]
         :source-paths ["/home/vagrant/.lein/src"]
         :injections [;; humane-test-output makes clojure.test output more better
                      (require 'pjstadig.humane-test-output)
@@ -11,14 +10,11 @@
 
                      ;; Spyscope is for tracing.
                      (require 'spyscope.core)]
-        :dependencies [[org.clojure/java.classpath "0.2.2"]
-                       [org.clojure/tools.namespace "0.2.4"]
-                       [org.clojure/tools.trace "0.7.6"]
-                       [evalive "1.1.0"]
+        :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                       [org.clojure/tools.nrepl "0.2.7"]
+                       [org.clojure/tools.trace "0.7.8"]
                        [pjstadig/humane-test-output "0.7.0"]
                        [slamhound "1.5.5"]
-                       ;; [leiningen #= (leiningen.core.main/leiningen-version)]
-                       ;; [im.chit/vinyasa "0.2.0"]
                        [spyscope "0.1.5"]]}}
 
 ;;;; WIP: more emacs-like Gorilla keybindings.
