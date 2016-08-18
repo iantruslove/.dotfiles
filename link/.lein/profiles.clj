5163bbec8454143d44c26cb4130447d4f92020a0
Ian Truslove <ian.truslove@gmail.com>
2016-08-18 11:47:35 -0600

    Lein updates for newer ciders

--- link/.lein/profiles.clj
+++ link/.lein/profiles.clj
@@ -1,19 +1,24 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :plugins [[cider/cider-nrepl "0.10.0"]
-                  [refactor-nrepl "1.1.0"]
-                  [lein-pprint "1.1.1"]
+        :plugins [[lein-pprint "1.1.1"]
                   [lein-bikeshed "0.2.0"]
-                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]]]
-        :source-paths ["/home/vagrant/.lein/src"]
+                  [lein-auto "0.1.2"]
+                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]]
+                  [jonase/eastwood "0.2.3"]]
+
         :injections [;; humane-test-output makes clojure.test output more better
-                     (require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)
+                     ;; ...except when it's dealing with doubles...
+                     ;; (require 'pjstadig.humane-test-output)
+                     ;; (pjstadig.humane-test-output/activate!)
 
                      ;; Spyscope is for tracing.
                      (require 'spyscope.core)]
+
         :dependencies [[org.clojure/tools.namespace "0.2.11"]
                        [org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/tools.trace "0.7.8"]
-                       [pjstadig/humane-test-output "0.7.0"]
+                       [pjstadig/humane-test-output "0.8.0"]
                        [slamhound "1.5.5"]
-                       [spyscope "0.1.5"]]}}
+                       [spyscope "0.1.5"]]}
+
+ :repl {:plugins [[cider/cider-nrepl "0.12.0"]
+                  [refactor-nrepl "2.2.0"]]}}
